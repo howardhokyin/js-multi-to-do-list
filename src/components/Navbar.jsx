7e7175e0ae25f8c6a29a3d1876dfdbe1e5a1f1d9
howardhokyin <howard.hyca@gmail.com>
2024-06-10 15:16:12 -0600

    changed Navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,10 @@ const NavBar = () => {
     <div className="flex items-center gap-5 bg-slate-200 p-3">
       <h1 className="flex-1 text-left font-bold">To Do List</h1>
       <nav className="flex-1 flex space-x-5 font-semibold">
-        <Link to="/" className="hover:underline">
+        <Link to="/" className="hover:bg-blue-400">
           Home
         </Link>
-        <Link to="/login" className="hover:underline">
+        <Link to="/login" className="hover:bg-blue-400">
           Login
         </Link>
       </nav>
